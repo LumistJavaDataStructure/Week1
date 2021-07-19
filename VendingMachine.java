cdf2ccc1aa7f2be0b5a41dd61d67c293647531a7
Steven Yuan <68981504+StevenYuan666@users.noreply.github.com>
2021-07-19 14:48:23 +0800

    Assignment1 Template

---
+++ VendingMachine.java
@@ -0,0 +1,38 @@
+//Do not import any other package you don't need at all
+
+import java.util.Scanner;
+
+public class VendingMachine {
+    //TODO: Declare the static variables here
+
+    public static void displayMenu(){
+        //TODO: Display the welcome menu
+    }
+
+    public static int getSnackPrice(int choice){
+        //TODO: return the price in cent for choice 1 - 4, and return 0 otherwise
+        return 0;
+    }
+
+    public static int getNumOfCoins(int amountNeeded, int valueOfCoin, int coinsAvailable){
+        //TODO: return the maximum number of coins we could change to the customer
+        return 0;
+    }
+
+    public static boolean computeAndDisplayChange(int change){
+        //TODO: compute the changes and display the information if needed, return true if we are able to change
+        return true;
+    }
+
+    public static void operateMachine(){
+        //TODO: follow the instruction of assignment
+
+
+        //Hints: Use Scanner to read input from the user
+    }
+
+    public static void main(String[] args){
+        //Do not modify or delete the code here
+        operateMachine();
+    }
+}
